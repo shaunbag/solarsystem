4c0e96653507d5a8b14bdeef292a7f43a9538feb
Shaunbag <shaunbag@hotmail.com>
2021-10-16 11:24:14 +0100

    3d models mars and mercury added, react-router added

---
+++ src/components/Mars.js
@@ -0,0 +1,9 @@
+function Mars() {
+    return(
+        <div>
+         <iframe title="mars" src='https://solarsystem.nasa.gov/gltf_embed/2372' width='100%' height='450px' frameBorder='0' />
+        </div>
+    )
+}
+
+export default Mars;
---
+++ src/components/Mercury.js
@@ -0,0 +1,9 @@
+function Mercury() {
+    return(
+        <div>
+         <iframe title="mercury" src='https://solarsystem.nasa.gov/gltf_embed/2369' width='100%' height='450px' frameBorder='0' />
+        </div>
+    )
+}
+
+export default Mercury;
