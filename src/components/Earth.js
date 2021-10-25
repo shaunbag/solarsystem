fb0b1f2f0756678c50847697e69f6d2056442573
Shaunbag <shaunbag@hotmail.com>
2021-10-25 15:07:52 +0100

    styling, beginning planetary data

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -42,25 +42,41 @@ const Earth = (props) => {
     return(
         <div className="threeD">
             <div className="blurb">
-                <Link to="/"> <h1>The Planet {props.planet.englishName}</h1></Link>
-                <p>aphelion: {props.planet.aphelion}</p> 
-                <p>argPeriapsis: {props.planet.argPeriapsis}</p>
-                <p>avgTemp: {props.planet.avgTemp}</p>
-                <p>axialTilt: {props.planet.axialTilt}</p> 
-                <p>density: {props.planet.density}</p> 
-                <p>eccentricity: {props.planet.eccentricity}</p>
-                <p>englishName: {props.planet.englishName}</p>
-                <p>equaRadius: {props.planet.equaRadius}</p>
-                <p>escape: {props.planet.escape}</p> 
-                <p>flattening: {props.planet.flattening}</p> 
-                <p>gravity:  {props.planet.gravity}</p>
-                <p>inclination: {props.planet.inclination}</p> 
-                <p>longAscNode:  {props.planet.longAscNode}</p>
-                <p>mainAnomaly: {props.planet.mainAnomaly}</p>
+            <Link to="/"><h1 className="title">{props.planet.englishName}</h1></Link>
+                <div className="info">
+                    <div>
+                        <p>
+                        Earth is the third planet from the Sun and the only astronomical object known to harbour and support life.
+                         About 29.2% of Earth's surface is land consisting of continents and islands. 
+                         The remaining 70.8% is covered with water, mostly by oceans, seas, gulfs, and other salt-water bodies, 
+                         but also by lakes, rivers, and other freshwater, which together constitute the hydrosphere. 
+                         Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic 
+                         plates that migrate across the surface over many millions of years, while its interior remains active 
+                         with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, 
+                         and a convective mantle that drives plate tectonics.
+                        </p>
+                    </div>
+                    <div>
+                        <p>aphelion: {props.planet.aphelion}<br></br>
+                        argPeriapsis: {props.planet.argPeriapsis}<br></br>
+                        avgTemp: {props.planet.avgTemp}<br></br>
+                        axialTilt: {props.planet.axialTilt}<br></br>
+                        density: {props.planet.density}<br></br>
+                        eccentricity: {props.planet.eccentricity}<br></br>
+                        equaRadius: {props.planet.equaRadius}<br></br>
+                        escape: {props.planet.escape}<br></br>
+                        flattening: {props.planet.flattening}<br></br>
+                        gravity:  {props.planet.gravity}<br></br>
+                        inclination: {props.planet.inclination}<br></br>
+                        longAscNode:  {props.planet.longAscNode}<br></br>
+                        mainAnomaly: {props.planet.mainAnomaly}</p>
+                    </div>
+                </div>
             </div>
         
          <iframe title="earth" src={iframe} width='100%' height='450px' frameBorder='0' />
         </div>
+        
     )
 }
 
